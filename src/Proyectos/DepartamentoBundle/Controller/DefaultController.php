8ace019c5a2b27fdcd822b7221418d20f9436ab7
Pablo <pablortega83@hotmail.com>
2014-04-02 09:33:13 +0200

    Creacion Bundle

---
+++ src/Proyectos/DepartamentoBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Proyectos\DepartamentoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('ProyectosDepartamentoBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
