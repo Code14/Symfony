6b2e2843ae6f965d45c05c80bc441ac15cc6ac78
Pablo <pablortega83@hotmail.com>
2014-04-07 13:52:44 +0200

    Creacion de proyecto Departamento

---
+++ src/Proyectos/DepartamentoBundle/Controller/AlumnosController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Proyectos\DepartamentoBundle\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Proyectos\DepartamentoBundle\Entity\Alumno;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AlumnosController extends Controller
+{
+    public function indexAction()
+    {
+        $alumnos = $this->getDoctrine()->getRepository('ProyectosDepartamentoBundle:Alumno')->findAll();
+
+        // Nombre del array con el que pasamos los datos a la plantilla twig
+        return $this->render('ProyectosDepartamentoBundle:Alumnos:index.html.twig', array('data' => $alumnos));
+    }
+}
--- src/Proyectos/DepartamentoBundle/Controller/DefaultController.php
+++ src/Proyectos/DepartamentoBundle/Controller/DefaultController.php
@@ -9,6 +9,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+
         return $this->render('ProyectosDepartamentoBundle:Default:index.html.twig');
+
     }
+
+
 }
